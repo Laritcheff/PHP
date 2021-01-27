ae27115ba36ce9f998bc60709c2c93033624864f
Laritcheff <68246179+Laritcheff@users.noreply.github.com>
2021-01-27 10:17:16 +0300

    Update 1.php

--- 1.php
+++ 1.php
@@ -1,4 +1,5 @@
 <?php
+/*простые числа*/
     function findSimple($a, $b){
         for($i=0;$a<=$b;$i++){           
             $simple[]=$a;                       
@@ -6,19 +7,30 @@
         }  $count=0;
             foreach($simple as $v){
                         for($k=2;$k<$b;$k++){
-                      if(($v%$k==0)&&($v!=$k)){                            
+                        if(($v%$k==0)&&($v!=$k)){                            
                         unset($simple[$count]);
-                            /*echo $v.'<br>';*/}  
-                        }  
-                            $count++;                               
+                                       }
+               $count++;                               
                         }
-                          return $simple;
-    
-    }
+                          return $simple;   
+    }  
+    } 
+     for($i=1;$i<13;$i++){
+            $d[]=$i;}
             
-    
-       echo findSimple(1,1250);
-           
- 
+
+         function createTrapeze($a){
+                $j=0;
+                $count=count($a)/3;    
+               for($i=0;$i<$count;$i++) { 
+                    $out=array_slice($a,$j,3);                                        
+                    $array[]=["a"=>$out[0],"b"=>$out[1],"c"=>$out[2]];              
+                    $j+=3;                                       
+                   }                  
+                foreach($array as $key=>$e){echo "$key=$e[a]<br>"; 
+         }               
+         }               
+         
+            createTrapeze($d);
 
 ?>
